bf1bb4f2cc8ad037a23383f1251310b76c4978c0
pengfu <77276575@qq.com>
2017-11-04 14:17:37 +0800

    添加TAG按钮的选中状态

--- app/src/main/java/com/popfu/gfbashain/MainActivity.java
+++ app/src/main/java/com/popfu/gfbashain/MainActivity.java
@@ -59,26 +59,47 @@ public class MainActivity extends FragmentActivity {
     public void afterViews(){
         L.d("afterViews");
         mViewPager.setAdapter(mainPagerAdapter);
+        // 因为默认显示是第一个页面，所以第一个按钮的状态变为选中状态
+        updateTabState(tabImages.get(0)) ;
+
     }
 
     @Click(R.id.iv_tab_in)
-    public void clickModuleIn(){
+    public void clickModuleIn(ImageView imageView){
+        L.d("click0:"+imageView);
         mViewPager.setCurrentItem(0);
+        updateTabState(imageView) ;
     }
 
     @Click(R.id.iv_tab_shai)
-    public void clickModuleShai(){
+    public void clickModuleShai(ImageView imageView){
+        L.d("click1:"+imageView);
         mViewPager.setCurrentItem(1);
+        updateTabState(imageView) ;
     }
 
     @Click(R.id.iv_tab_buy)
-    public void clickModuleBuy(){
+    public void clickModuleBuy(ImageView imageView){
+        L.d("click2:"+imageView);
         mViewPager.setCurrentItem(2);
+        updateTabState(imageView) ;
     }
 
     @Click(R.id.iv_tab_me)
-    public void clickModuleMe(){
+    public void clickModuleMe(ImageView imageView){
+        L.d("click3:"+imageView);
         mViewPager.setCurrentItem(3);
+        updateTabState(imageView) ;
+    }
+
+    private void updateTabState(ImageView clickView){
+        for(ImageView tabView : tabImages){
+            if(clickView == tabView){
+                tabView.setSelected(true);
+            }else{
+                tabView.setSelected(false);
+            }
+        }
     }
 
     class MainPagerAdapter extends FragmentPagerAdapter {
